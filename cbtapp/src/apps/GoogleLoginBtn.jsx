dd850b536abd58ad2cfbfff933626042e5e09a93
james lee <james.lee@devncore.org>
2021-12-04 22:00:05 +0900

    Update

--- cbtapp/src/apps/GoogleLoginBtn.jsx
+++ cbtapp/src/apps/GoogleLoginBtn.jsx
@@ -1,45 +1,14 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 
-const clientId = "130873093876-6hba2is1i9520ip6i4jm8kacfq95fbn6.apps.googleusercontent.com";
+const clientId = "130873093876-0q51vs5cct76oddjfq4gv72caaur8009.apps.googleusercontent.com";
 
 export default function GoogleLoginBtn({ onGoogleLogin }){
-
-
-    function onSubmit() {
-        
-        const data = {
-            "userId": "string",
-            "userName": "string",
-            "email": "string",
-            "loginProvider": "string",
-            "providerDisplayName": "string",
-            "securityStamp": "string",
-            "concurrencyStamp": "string"
-        }
-
-        // POST request using fetch inside useEffect React hook
-        const requestOptions = {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        };
-
-        fetch('/api/account/login', requestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
-
-        // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    }
 
     const onSuccess = async(response) => {
         const { googleId, profileObj : { email, name } } = response;
         console.log('successed!', response);
         console.log('ba: ', response["Ba"]);
-        onSubmit();
     }
 
     const onFailure = (error) => {
@@ -53,6 +22,7 @@ export default function GoogleLoginBtn({ onGoogleLogin }){
                 responseType={"id_token"}
                 onSuccess={onSuccess}
                 onFailure={onFailure}/>
+
         </div>
     )
 }
--- cbtapp/src/apps/Login.jsx
+++ cbtapp/src/apps/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import GoogleLoginBtn from './GoogleLoginBtn.jsx';
 
 export default function Login(props) {
-
     return (
         <div>
             <h1>Login</h1>
