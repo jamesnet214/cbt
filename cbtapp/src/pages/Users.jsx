d17fefcdfa48fa61a631bf66ac46754298a7c925
james lee <james.lee@devncore.org>
2021-12-05 01:05:26 +0900

    Update profile

--- cbtapp/src/pages/Users.jsx
+++ cbtapp/src/pages/Users.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default function Users(props) {
-    
+
     return (
         <div>
             USERS
+            이름
+            이메일
+            프로필이미지            
         </div>
     );
 }
