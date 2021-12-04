423231641fae2935e205e39c79abfb4803d542ce
james lee <james.lee@devncore.org>
2021-12-05 02:13:39 +0900

    Update users

--- cbtapp/src/pages/Users.jsx
+++ cbtapp/src/pages/Users.jsx
@@ -1,6 +1,45 @@
-import React from 'react';
-
+import React from "react";
+import Axios from "axios";
 export default function Users(props) {
+
+    React.useEffect(() => {
+
+        const data = {
+            "id": "5451",
+            "userName": "string",
+            "email": "string"
+        };
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };
+
+        Axios.post('/api/Account/getLoginInfo', data)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        // fetch('https://ncoreapi.azurewebsites.net/api/Account/getLoginInfo', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(data)
+        //     });
+
+        // fetch('https://ncoreapi.azurewebsites.net/api/Account/getLoginInfo', requestOptions)
+        //     .then(response => response.json())
+        //     .then(data => console.log(data));
+
+        // Axios.post('https://ncoreapi.azurewebsites.net/api/Account/getLoginInfo', data)
+        //     .then(response => console.log(response));
+    });
 
     return (
         <div>
