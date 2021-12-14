72156c11bf3359e23d4ff07261b8e9fdac46f920
james.lee <james.lee@devncore.org>
2021-12-15 02:39:37 +0900

    Update externals

--- cbtapp/src/pages/Settings.jsx
+++ cbtapp/src/pages/Settings.jsx
@@ -4,7 +4,8 @@ import Cookies from 'universal-cookie';
 import Button from "@mui/material/Button";
 
 export default function Settings(props) {
-    const [userInfo, setUserInfo] = React.useState(null);
+    const [userInfo, setUserInfo] = React.useState({});
+    const [externals, setExternals] = React.useState([]);
     const cookies = new Cookies();
     const token = cookies.get('.cbt.devncore.org.authentication.session');
     
@@ -23,22 +24,17 @@ export default function Settings(props) {
             .then(function (response) {
                 const userData = response.data;
                 setUserInfo({
-                        id: userData.id,
-                        userName: userData.userName,
-                        userId: userData.UserId,
-                        email: userData.email,
-                        phone: userData.phone,
-                        name: userData.name,
-                        school: userData.school,
-                        gitHubId: userData.gitHubId,
-                        blog: userData.blog,
-                        aboutMe: userData.aboutMe,
-                        externals: userData.externals.map( ext => {
-                            return {
-                                loginProvider: ext.loginProvider,
-                                providerKey: ext.providerKey
-                            }
-                        })
+                    id: userData.id,
+                    userName: userData.userName,
+                    userId: userData.UserId,
+                    email: userData.email,
+                    phone: userData.phone,
+                    name: userData.name,
+                    school: userData.school,
+                    gitHubId: userData.gitHubId,
+                    blog: userData.blog,
+                    aboutMe: userData.aboutMe,
+                    externals: []
 
 
 
@@ -46,10 +42,16 @@ export default function Settings(props) {
                         // providerKey: responseData.providerKey
                 });
 
-                
+                let _externals = userData.externals.map( ext => {
+                    return {
+                        loginProvider: ext.loginProvider,
+                        providerKey: ext.providerKey
+                    }
+                })
 
-                console.log("여기");
-                console.log(response.data);
+                setExternals(_externals);
+
+                console.log("여기", _externals);
           })
           .catch(function (error) {
             console.log(error);
@@ -59,40 +61,39 @@ export default function Settings(props) {
 
     return (
         <div className={"hstyle"}>
-            <div>
-                <div className={"stylestxt1"}>My Profile</div>
-                <div className={"stylestxt2"}>ID</div>
-                <div className={"stylestxt3"}>{userInfo.id}</div>
-                <div className={"stylestxt2"}>Name</div>
-                <div className={"stylestxt3"}>{userInfo.userName}</div>
-                <div className={"stylestxt2"}>Phone</div>
-                <div className={"stylestxt3"}>{userInfo.phone}</div>
-                <div className={"stylestxt2"}>Email</div>
-                <div className={"stylestxt3"}>{userInfo.email}</div>
-                <div className={"stylestxt2"}>School</div>
-                <div className={"stylestxt3"}>{userInfo.school}</div>
-                <div className={"stylestxt2"}>Github ID</div>
-                <div className={"stylestxt3"}>{userInfo.gitHubId}</div>
-                <div className={"stylestxt2"}>BlogURL</div>
-                <div className={"stylestxt3"}>{userInfo.blog}</div>
-                <div className={"stylestxt2"}>AboutMe</div>
-                <div className={"stylestxt3"}>{userInfo.aboutMe}</div>
-                {userInfo.externals != null ? 
-                    <div>{userInfo.externals.map( ext => {
-                        <div>
-                            <div>{ext.loginProvider}</div>
-                            <div>{ext.providerKey}</div>
-                        </div>
-                    })} 
-                    </div> 
-                    : null
-                }
-                <Button children="저장"/>
-            </div>
-            
+            {userInfo != null ?
 
-            {/* <div>{userInfo.loginProvider}</div>
-            <div>{userInfo.providerKey}</div> */}
+                <div>
+                    <div className={"stylestxt1"}>My Profile</div>
+                    <div className={"stylestxt2"}>ID</div>
+                    <div className={"stylestxt3"}>{userInfo.id}</div>
+                    <div className={"stylestxt2"}>Name</div>
+                    <div className={"stylestxt3"}>{userInfo.userName}</div>
+                    <div className={"stylestxt2"}>Phone</div>
+                    <div className={"stylestxt3"}>{userInfo.phone}</div>
+                    <div className={"stylestxt2"}>Email</div>
+                    <div className={"stylestxt3"}>{userInfo.email}</div>
+                    <div className={"stylestxt2"}>School</div>
+                    <div className={"stylestxt3"}>{userInfo.school}</div>
+                    <div className={"stylestxt2"}>Github ID</div>
+                    <div className={"stylestxt3"}>{userInfo.gitHubId}</div>
+                    <div className={"stylestxt2"}>BlogURL</div>
+                    <div className={"stylestxt3"}>{userInfo.blog}</div>
+                    <div className={"stylestxt2"}>AboutMe</div>
+                    <div className={"stylestxt3"}>{userInfo.aboutMe}</div>
+                    <div>{externals.map( ext => {
+                        return (
+                            <div>
+                                <div>{ext.loginProvider}</div>
+                                <div>{ext.providerKey}</div>
+                            </div>
+                        );
+                    })} 
+                    </div>
+                    
+                    <Button children="저장"/>
+                </div>
+            : null}          
             
         </div>
     );
