bea96174aa255eda3b4001be86170bf6b65c25e2
james.lee <james.lee@devncore.org>
2021-12-21 02:13:48 +0900

    Update test filter

--- cbtapp/src/pages/Test.jsx
+++ cbtapp/src/pages/Test.jsx
@@ -88,7 +88,7 @@ export default function Test(props) {
 
         console.log("결과보기 시작.");
 
-        text.map((item, index) => {
+        text.filter(x=>subjects.includes(x.subject)).slice(0, testCount).map((item, index) => {
             return console.log(`${index}: ${item.question}`, item);
         });
     }
