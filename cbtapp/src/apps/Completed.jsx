13cef3e27f62b659a251bda41bfdc1d931a1272f
kevin.kim <kevin.kim@devncore.org>
2021-12-29 22:33:32 +0900

    이미 가입된 계정 연결시 예외처리

--- cbtapp/src/apps/Completed.jsx
+++ cbtapp/src/apps/Completed.jsx
@@ -8,6 +8,11 @@ export default function Completed(props) {
     const cookies = new Cookies();
     const location = useLocation();
     let id = new URLSearchParams(location.search).get('id');
+    let isduplicate = new URLSearchParams(location.search).get('duplicate');
+    if (isduplicate == "true")
+    {
+        alert("이미 가입된 계정은 연결할 수 없습니다.");
+    }
     console.log('ID', id);
 
     cookies.set('.cbt.devncore.org.authentication.session', id);
